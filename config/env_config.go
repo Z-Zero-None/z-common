f5acc78d7bf9e73d8181f51af07dfe28395c5d0d
Z-Zero-None <1006746222@qq.com>
2022-01-26 22:37:04 +0800

    env struct

--- config/env_config.go
+++ config/env_config.go
@@ -1,1 +1,10 @@
 package config
+/*
+	read file(.env) var to set env(linux) var
+*/
+
+type EnvOptions struct{
+	FileType string		//file type. such as yaml/json/env etc.
+	FilePath string		//file path. runtime directory as root.
+	EnvPrefix string	//env variable pre. such as preXXX
+}
