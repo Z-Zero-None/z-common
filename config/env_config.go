691b45a4feb927b6b565c26d29edde2d8805198c
dlrow olleh <1006746222@qq.com>
2022-01-26 22:48:04 -0800

    env

--- config/env_config.go
+++ config/env_config.go
@@ -1,10 +1,7 @@
 package config
-/*
-	read file(.env) var to set env(linux) var
-*/
 
 type EnvOptions struct{
-	FileType string		//file type. such as yaml/json/env etc.
-	FilePath string		//file path. runtime directory as root.
-	EnvPrefix string	//env variable pre. such as preXXX
+	FileType string
+	FilePath string
+	EnvPrefix string
 }
