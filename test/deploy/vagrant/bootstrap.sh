4e3a66fecbea1bf911c6cc5b4f1b65f5d6f29ba1
zhognzn <1006746222@qq.com>
2023-08-05 12:31:32 +0800

    bootstrap文件调整

--- test/deploy/vagrant/bootstrap.sh
+++ test/deploy/vagrant/bootstrap.sh
@@ -17,6 +17,24 @@ cat >>/etc/hosts<<EOF
 192.168.56.10 k8s-master
 192.168.56.11 k8s-node1
 192.168.56.12 k8s-node2
+52.74.223.119 github.com
+192.30.253.119 gist.github.com
+54.169.195.247 api.github.com
+185.199.111.153 assets-cdn.github.com
+151.101.64.133 raw.githubusercontent.com
+151.101.108.133 user-images.githubusercontent.com
+151.101.76.133 gist.githubusercontent.com
+151.101.76.133 cloud.githubusercontent.com
+151.101.76.133 camo.githubusercontent.com
+151.101.76.133 avatars0.githubusercontent.com
+151.101.76.133 avatars1.githubusercontent.com
+151.101.76.133 avatars2.githubusercontent.com
+151.101.76.133 avatars3.githubusercontent.com
+151.101.76.133 avatars4.githubusercontent.com
+151.101.76.133 avatars5.githubusercontent.com
+151.101.76.133 avatars6.githubusercontent.com
+151.101.76.133 avatars7.githubusercontent.com
+151.101.76.133 avatars8.githubusercontent.com
 EOF
 cat /etc/hosts
 
@@ -47,7 +65,7 @@ cat >>/etc/docker/daemon.json<<EOF
       ]
 }
 EOF
-systemctl restart docker 
+systemctl restart docker
 docker -v
 
 #配置请求网桥
@@ -55,6 +73,7 @@ echo "[STEP4] IPTABLES SETTING"
 cat >>/etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward=1
 EOF
 sysctl --system >/dev/null 2>&1
 
@@ -82,10 +101,15 @@ systemctl enable kubelet >/dev/null 2>&1
 } >/dev/null 2>&1
 systemctl restart kubelet
 
-#解决containerd默认不适用cri
+#解决containerd默认不适用cri+替换镜像选用
 echo "[STEP8] CONTAINERD ENABLE"
+sed -i "s#k8s.gcr.io#registry.cn-hangzhou.aliyuncs.com/google_containers#g"  /etc/containerd/config.toml
+#sed -i "s#https://registry-1.docker.io#https://registry.cn-hangzhou.aliyuncs.com#g"  /etc/containerd/config.toml
 sed -i 's/^disabled_plugins = \["cri"\]/#&/' /etc/containerd/config.toml
 systemctl restart containerd
+#解决crictl images 出现报错问题"ListImages with filter from image service failed"
+crictl config runtime-endpoint unix:///run/containerd/containerd.sock
+crictl config image-endpoint unix:///run/containerd/containerd.sock
 
 #解决证书过期问题
 echo "[STEP9] CERTIFICATE EXPIRED"
@@ -96,7 +120,7 @@ apt install -y software-properties-common \
 apt install -y ipset ipvsadm conntrack socat
 apt-get update
 
-# kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.56.10 --image-repository registry.aliyuncs.com/google_containers 
+# kubeadm init --pod-network-cidr=10.244.0.0/16 --kubernetes-version=v1.27.3 --apiserver-advertise-address=192.168.56.10 --image-repository registry.aliyuncs.com/google_containers
 
 #安装go相关
 VERSION=1.18 
@@ -112,3 +136,6 @@ export GOPATH=$GOPATH
 export PATH=\$PATH:\$GOPATH/bin
 EOF
 source /etc/profile
+
+#echo -e "y" |sudo ufw enable
+#ufw allow 6443/tcp
