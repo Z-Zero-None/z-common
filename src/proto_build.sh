c3c302f8e00b29d353c60faef0fbe913a4a79167
dlrow olleh <1006746222@qq.com>
2024-06-19 12:08:26 +0800

    protoc-gateway: gen.sh

--- src/proto_build.sh
+++ src/proto_build.sh
@@ -9,7 +9,7 @@ do
     then
         dir_name=$(basename $file)
         if [ $dir_name != "ptypes" -a $dir_name != "errors"  -a $dir_name != "games" ]; then
-            protoc --go_out=. --go_opt=paths=import --go-grpc_out=. --go-grpc_opt=require_unimplemented_servers=false,paths=import api/${dir_name}/src/*.proto
+               protoc  -I . --go_out=. --go_opt=paths=import --go-grpc_out=. --go-grpc_opt=require_unimplemented_servers=false,paths=import -I ./api/thirdparty  --grpc-gateway_out=. --grpc-gateway_opt=paths=import  api/${dir_name}/src/*.proto
         fi
     fi
 done
